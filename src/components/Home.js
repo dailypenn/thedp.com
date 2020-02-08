7460c321798754578a675c94d0e20313c7b6ad3c
peterbaile <peterchenwflms@gmail.com>
2020-02-08 16:23:56 -0500

    add home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import { Row, Col, Container, Image } from 'react-bootstrap'
 
 import Layout from './layout'
 import SEO from './seo'
@@ -33,8 +34,21 @@ const Home = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1> Created a Page </h1>
-      <img src={`https://snworksceo.imgix.net/dpn/${attachment_uuid}.sized-1000x1000.jpg?w=1000`} />
+      <Container>
+        <Row>
+          <Col xs={7}>
+            <Row>
+              <Col>
+              </Col>
+              <Col>
+              1 of 1
+                {/* <Image src={`https://snworksceo.imgix.net/dpn/${attachment_uuid}.sized-1000x1000.jpg?w=1000`} fluid/> */}
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={5}> 2 of 3 </Col>
+        </Row>
+      </Container>
     </Layout>
   )
 }
