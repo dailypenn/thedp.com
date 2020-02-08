23a2153771973d9e74ee44cb9903d2ff28760dc3
Daniel Tao <dtao@seas.upenn.edu>
2020-02-08 18:07:13 -0500

    update button styling and add nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react"
 import s from "styled-components"
-import { Button, Dropdown, Image, Row, Navbar } from "react-bootstrap"
+import { Button, Nav, NavDropdown, Image, Row, Navbar } from "react-bootstrap"
 import Logo from '../images/icons/header-logo.svg'
 
-
-// import SVGs from /image folder
-import FBSVG from '../images/icons/facebook.svg'
 
 const TopRow = s(Row)`
   text-align: center;
   padding: 20px 0 10px;
   background: #ffffff;
   justify-content: center;
-
 `
 
 const HeaderLogo = s(Logo)`
@@ -33,6 +29,7 @@ const HeaderLogoText = s.p`
   text-align: right;
   color: default;
   text-decoration: none;
+  font-size: 14px;
 `
 
 const HeaderLogoWrapper = s.div`
@@ -47,21 +44,54 @@ const ButtonWrapper = s.div`
   flex-direction: column;
 `
 
-const TopButton = s(Button)`
+const NewsButton = s(Button)`
   border-radius: 5px;
   font-family: "Heebo", sans-serif;
   margin-bottom: 10px;
   font-weight: bold;
+  font-size: 14px !important;
+  padding: 3px 12px !important;
+  color: #fff !important;
+  background-color: #aa1e22 !important;
+  border-color: #941a1e !important;
+`
+
+const DonateButton = s(Button)`
+  border-radius: 5px;
+  font-family: "Heebo", sans-serif;
+  margin-bottom: 10px;
+  font-weight: bold;
+  font-size: 14px !important;
+  padding: 3px 12px !important;
+  color: #aa1e22 !important;
+  background-color: #fff !important;
+  border-color: #941a1e !important;
+  border-width: 2px !important;
+`
+
+const HeaderNavbar = s(Navbar)`
+  border-top: 1px solid #A9A9A9;
+  border-bottom: 1px solid #A9A9A9;
+  padding: 0 !important;
+  width: 100%;
 `
 
 const NavbarContainer = s.div`
   margin-left: 20px;
   margin-right: 20px;
+  max-height: 48px;
 `
 
-const NavDate = s.div`
+const NavDate = s(Nav.Link)`
   border-right: 2px solid #e7e7e7;
   padding-right: 10px;
+  color: black !important;
+  font-family: "Heebo", sans-serif;
+`
+
+const NavLink = s(Nav.Link)`
+  color: black !important;
+  font-family: "Heebo", sans-serif;
 `
 
 // TODO: Make mcModal trigger a modal
@@ -75,40 +105,38 @@ const Header = () => {
         <HeaderLogoWrapper href="/">
           <HeaderLogo />
           <HeaderLogoText>
-          Founded in 1885
+            Founded in 1885
           </HeaderLogoText>
         </HeaderLogoWrapper>
         <ButtonWrapper>
-          <TopButton variant="primary" size="sm">
+          <NewsButton variant="primary" size="sm">
             NEWSLETTERS
-            </TopButton>
-          <TopButton variant="inverse" size="sm">
+            </NewsButton>
+          <DonateButton variant="inverse" size="sm">
             DONATE
-            </TopButton>
+          </DonateButton>
         </ButtonWrapper>
       </TopRow>
-      <Navbar>
+      <HeaderNavbar>
         <NavbarContainer>
           <Navbar.Collapse>
-            <NavDate>
-              February 8th, 2020
-            </NavDate>
-            <Dropdown>
-              <Dropdown.Toggle>
-                Dropdown Button
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Nav>
+              <NavDate>
+                February 8th, 2020
+              </NavDate>
+              <NavLink href="">NEWS</NavLink>
+              <NavLink href="">SPORTS</NavLink>
+              <NavLink href="">OPINION</NavLink>
+              <NavLink href="">PROJECTS</NavLink>
+              <NavLink href="">34TH STREET</NavLink>
+              <NavLink href="">UNDER THE BUTTON</NavLink>
+            </Nav>
           </Navbar.Collapse>
         </NavbarContainer>
       </Navbar>
-  </header>
-)
+      </HeaderNavbar>
+    </header>
+  )
 }
 
 
