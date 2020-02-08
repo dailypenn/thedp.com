fdd3814e19c37c97369c2f886dea4e34dae5f4a7
peterbaile <peterchenwflms@gmail.com>
2020-02-08 14:59:21 -0500

    fix bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,15 @@ const BASE_URL = 'http://localhost:5000/fetch?url='
 const Home = () => {
   const [centerpiece, setCenterpiece] = useState([])
   useEffect(() => {
-    const resp = axios.get(`${BASE_URL}http://www.thedp.com/section/centerpiece.json`)
+    axios.get(`${BASE_URL}http://www.thedp.com/section/centerpiece.json`)
       .then(resp => {
-        const { articles } = resp
-        setCenterpiece[articles[0]]
+        const { articles } = resp.data
+        setCenterpiece(articles[0])
       })
   }, [])
 
   return (
-    <h1> Hello </h1>
+    <h1> { centerpiece.headline } </h1>
   )
 }
 
