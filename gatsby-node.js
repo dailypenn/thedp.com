868a1bed4cb2192a9611f8745742bff4a25cd24d
peterbaile <peterchenwflms@gmail.com>
2020-02-10 21:16:12 -0500

    add templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,11 +10,10 @@ const path = require('path')
 const axios = require('axios')
 
 const BASE_URL = 'http://localhost:5000/fetch?url='
-const HomeTemplate = path.resolve('./src/components/Home.js')
+const HomeTemplate = path.resolve('./src/templates/HomePage.js')
 
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-
+// TODO: add action from create HomePage
+const createHomePage = async (createPage) => {
   const resp = await axios.get(`${BASE_URL}http://www.thedp.com/section/centerpiece.json`)
 
   const topResp = await axios.get(`${BASE_URL}http://www.thedp.com/section/top.json`)
@@ -40,4 +39,33 @@ exports.createPages = async ({ actions }) => {
       mostReadUTB: mostUTBResp.data.result.slice(0, 3),
     }
   })
+}
+
+// TODO: add action for creating section pages
+
+// TODO: add action for creating article pages
+const createArticles = async (createPage) => {
+  const newsResp = await axios.get(`${BASE_URL}https://www.thedp.com/section/news.json`)
+  newsResp.data.articles.forEach(article => {
+    createPage({
+      path: '/',
+      component: HomeTemplate,
+      context: {
+        centerpiece: articles[0],
+        topArticles: topArticles.slice(0,3),
+        mostReadDP: mostReadDPResp.data.result.slice(0, 5),
+        mostRead34: most34Resp.data.result.slice(0, 3),
+        mostReadUTB: mostUTBResp.data.result.slice(0, 3),
+      }
+    })
+  })
+}
+
+
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+
+  await createHomePage(createPage)
+  await createArticles(createPage)
 }
--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+// import { graphql } from 'gatsby'
 import { Row, Col, Container, Image, Card, ListGroup } from 'react-bootstrap'
 import s from 'styled-components'
 
-import Layout from './layout'
-import SEO from './seo'
-import Footer from './Footer'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Footer from '../components/Footer'
 
 const SubHeader = s.h3`
   color: #aa1e22;
@@ -32,61 +32,19 @@ const MostReadCard = ({ idx, content }) => {
   return (
     <Row>
       <Col xs={3} style={{ color: '#AA1E22', fontSize: '36px' }}> {idx} </Col>
-      <Col> <p dangerouslySetInnerHTML={{ __html: content }} style={{ fontSize: '80%' }}/> </Col>
+      <Col> <p dangerouslySetInnerHTML={{ __html: content }} style={{ fontSize: '80%' }} /> </Col>
     </Row>
   )
 }
 
-const Home = () => {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      sitePage(path: {eq: "/"}) {
-        path
-        context {
-          centerpiece {
-            abstract
-            content
-            dominantMedia {
-              content
-              attachment_uuid
-            }
-            headline
-            subhead
-          }
-          topArticles {
-            abstract
-            content
-            dominantMedia {
-              attachment_uuid
-              content
-            }
-            headline
-            subhead
-          }
-          mostReadDP {
-            path
-            image
-            ogTitle
-          }
-          mostRead34 {
-            path
-            image
-            ogTitle
-          }
-          mostReadUTB {
-            path
-            image
-            ogTitle
-          }
-        }
-      }
-    }
-  `)
-
-  const centerpieceData = data.sitePage.context.centerpiece
-  const topData = data.sitePage.context.topArticles
-  const mostReadDP = data.sitePage.context.mostReadDP
-  const { mostRead34, mostReadUTB } = data.sitePage.context
+const Home = ({ pageContext: context }) => {
+  const {
+    centerpiece: centerpieceData,
+    topArticles: topData,
+    mostReadDP,
+    mostRead34,
+    mostReadUTB
+  } = context
 
   const { dominantMedia, headline, abstract } = centerpieceData
   const { content, attachment_uuid } = dominantMedia
@@ -103,17 +61,17 @@ const Home = () => {
                 {topData.map(article => {
                   const { dominantMedia: { attachment_uuid }, headline } = article
                   return (
-                    <Row style={{ borderBottom: '1px solid #EBEBEB', paddingBottom:'1em', marginBottom: '1em', marginRight: '0.5em' }}>
+                    <Row style={{ borderBottom: '1px solid #EBEBEB', paddingBottom: '1em', marginBottom: '1em', marginRight: '0.5em' }}>
                       <Image fluid src={`https://snworksceo.imgix.net/dpn/${attachment_uuid}.sized-1000x1000.jpg?w=1000`} />
-                      <strong>{ headline }</strong>
+                      <strong>{headline}</strong>
                     </Row>
                   )
                 })}
               </Col>
               <Col>
-                <Image src={`https://snworksceo.imgix.net/dpn/${attachment_uuid}.sized-1000x1000.jpg?w=1000`} fluid/>
+                <Image src={`https://snworksceo.imgix.net/dpn/${attachment_uuid}.sized-1000x1000.jpg?w=1000`} fluid />
                 <h4> {headline} </h4>
-                <p dangerouslySetInnerHTML={{ __html: abstract }}/>
+                <p dangerouslySetInnerHTML={{ __html: abstract }} />
               </Col>
             </Row>
             <SubHeader> TRENDING </SubHeader>
@@ -124,17 +82,17 @@ const Home = () => {
             <div style={{ borderBottom: '1px solid #EBEBEB', padding: '1em 0', marginRight: '1em' }}>
               <div style={{ textAlign: 'center', padding: '0 5em' }}>
                 <img style={{ height: '300px' }} src={mostRead34[0].image} />
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[0].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[0].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </div>
             </div>
             <Row style={{ marginTop: '1em', borderBottom: '1px solid #A9A9A9', paddingBottom: '1em', marginRight: '1em' }}>
               <Col xs={6} style={{ fontSize: '90%', padding: '0 2em', borderRight: '1px solid #EBEBEB' }}>
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[1].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[1].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </Col>
               <Col style={{ fontSize: '90%', padding: '0 2em' }}>
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[2].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostRead34[2].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </Col>
             </Row>
@@ -143,17 +101,17 @@ const Home = () => {
             <div style={{ borderBottom: '1px solid #EBEBEB', padding: '1em 0', marginRight: '1em' }}>
               <div style={{ textAlign: 'center', padding: '0 5em' }}>
                 <img style={{ height: '300px' }} src={mostReadUTB[0].image} />
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[0].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[0].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </div>
             </div>
             <Row style={{ marginTop: '1em' }}>
               <Col xs={6} style={{ fontSize: '90%', padding: '0 2em', borderRight: '1px solid #EBEBEB' }}>
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[1].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[1].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </Col>
               <Col style={{ fontSize: '90%', padding: '0 2em' }}>
-                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[2].ogTitle }}/> </p>
+                <p> <strong dangerouslySetInnerHTML={{ __html: mostReadUTB[2].ogTitle }} /> </p>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
               </Col>
             </Row>
@@ -165,7 +123,7 @@ const Home = () => {
                 {mostReadDP.map((article, idx) => {
                   return (
                     <ListGroup.Item>
-                      <MostReadCard idx={idx+1} content={article.ogTitle} />
+                      <MostReadCard idx={idx + 1} content={article.ogTitle} />
                     </ListGroup.Item>
                   )
                 })}
@@ -180,5 +138,49 @@ const Home = () => {
     </Layout>
   )
 }
+
+// export const query = graphql`
+// query($slug: String!) {
+//   sitePage(path: {eq: $slug}) {
+//     path
+//     context {
+//       centerpiece {
+//         abstract
+//         content
+//         dominantMedia {
+//           content
+//           attachment_uuid
+//         }
+//         subhead
+//         headline
+//       }
+//       topArticles {
+//         abstract
+//         content
+//         dominantMedia {
+//           attachment_uuid
+//           content
+//         }
+//         headline
+//       }
+//       mostReadDP {
+//         path
+//         image
+//         ogTitle
+//       }
+//       mostRead34 {
+//         path
+//         image
+//         ogTitle
+//       }
+//       mostReadUTB {
+//         path
+//         image
+//         ogTitle
+//       }
+//     }
+//   }
+// }
+// `
 
 export default Home
