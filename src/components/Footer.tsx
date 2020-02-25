723463d819d4d5d6f3f061d3a4150791a40368b6
Alexander Go <alexanderd.go@gmail.com>
2020-02-24 22:29:02 -0500

    Clean-up Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Row, Col, Image } from 'react-bootstrap'
 import s from 'styled-components'
+import { AboutLinks, NewsLinks, SportsLinks, OpinionLinks, StreetLinks, UnderTheButtonLinks } from '../constants'
 
 const StyledLink = s(Link)`
+  color: #000000 !important;
+  text-decoration: none !important;
+`
+
+const StyledNonSlug = s.a`
   color: #000000 !important;
   text-decoration: none !important;
 `
@@ -34,70 +40,82 @@ const Footer = () => (
     </LogoWrapper>
     <Row>
       <Col>
-        <p><strong> <StyledLink to="#"> About </StyledLink> </strong></p>
-        <p> <StyledLink to="#"> About us </StyledLink> </p>
-        <p> <StyledLink to="#"> Archives </StyledLink> </p>
-        <p> Staff </p>
-        <p> Advertise </p>
-        <p> Donate </p>
-        <p> Apparel Store </p>
-        <p> Alumni Association </p>
-        <p> Privacy Policy </p>
-        <p> Terms of Service </p>
-        <p> Contact us </p>
+        {AboutLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledLink to={link.slug}>{link.section}</StyledLink></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledLink to={link.slug}>{link.section}</StyledLink></p>
+            )
+          }
+        })}
       </Col>
       <Col>
-        <p><strong> News </strong></p>
-        <p> Academics </p>
-        <p> Administration </p>
-        <p> Admissions </p>
-        <p> Crime </p>
-        <p> Politics </p>
-        <p> Philadelphia </p>
-        <p> <StyledLink to="#"> Student Life </StyledLink> </p>
-        <p> <StyledLink to="#"> Gender &amp; Diversity </StyledLink> </p>
+        {NewsLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledLink to={link.slug}>{link.section}</StyledLink></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledLink to={link.slug}>{link.section}</StyledLink></p>
+            )
+          }
+        })}
       </Col>
       <Col>
-        <p><strong> <StyledLink to="#"> Sports </StyledLink> </strong></p>
-        <p> <StyledLink to="#"> Men&#39;s Basketball </StyledLink> </p>
-        <p> <StyledLink to="#"> Women&#39;s Basketball </StyledLink> </p>
-        <p> <StyledLink to="#"> Wrestling </StyledLink> </p>
-        <p> <StyledLink to="#"> Men&#39;s Squash </StyledLink> </p>
-        <p> <StyledLink to="#"> Women&#39;s Squash </StyledLink> </p>
-        <p> <StyledLink to="#"> Men&#39;s Swimming </StyledLink> </p>
-        <p> <StyledLink to="#"> Women&#39;s Swimming </StyledLink> </p>
-        <p> <StyledLink to="#"> Track &amp; Field </StyledLink> </p>
-        <p> <StyledLink to="#"> Gymnastics </StyledLink> </p>
-        <p> <StyledLink to="#"> Fencing </StyledLink> </p>
+        {SportsLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledLink to={link.slug}>{link.section}</StyledLink></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledLink to={link.slug}>{link.section}</StyledLink></p>
+            )
+          }
+        })}
       </Col>
       <Col>
-        <p><strong> <StyledLink to="#"> Opinion </StyledLink> </strong></p>
-        <p> <StyledLink to="#"> Staff Editorials </StyledLink> </p>
-        <p> <StyledLink to="#"> Opinion Columns </StyledLink> </p>
-        <p> <StyledLink to="#"> Letters to the Editor </StyledLink> </p>
-        <p> <StyledLink to="#"> Opinion Art </StyledLink> </p>
-        <p> <StyledLink to="#"> Guent Columns </StyledLink> </p>
-        <p> <StyledLink to="#"> Submit a Column </StyledLink> </p>
+        {OpinionLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledLink to={link.slug}>{link.section}</StyledLink></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledLink to={link.slug}>{link.section}</StyledLink></p>
+            )
+          }
+        })}
       </Col>
       <Col>
-        <p><strong> <StyledLink to="#"> 34th Street </StyledLink> </strong></p>
-        <p> <StyledLink to="#"> Features </StyledLink> </p>
-        <p> <StyledLink to="#"> Ego </StyledLink> </p>
-        <p> <StyledLink to="#"> Word on the Street </StyledLink> </p>
-        <p> <StyledLink to="#"> Film &amp; TV </StyledLink> </p>
-        <p> <StyledLink to="#"> Music </StyledLink> </p>
-        <p> <StyledLink to="#"> Arts </StyledLink> </p>
-        <p> <StyledLink to="#"> Style </StyledLink> </p>
-        <p> <StyledLink to="#"> Overheads </StyledLink> </p>
-        <p> <StyledLink to="#"> Visual Stories </StyledLink> </p>
-        <p> <StyledLink to="#"> Projects </StyledLink> </p>
+        {StreetLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledNonSlug href={link.slug}>{link.section}</StyledNonSlug></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledNonSlug href={link.slug}>{link.section}</StyledNonSlug></p>
+            )
+          }
+        })}
       </Col>
       <Col>
-        <p><strong> <StyledLink to="#"> Under the Button </StyledLink> </strong></p>
-        <p> <StyledLink to="#"> News </StyledLink> </p>
-        <p> <StyledLink to="#"> Opinion </StyledLink> </p>
-        <p> <StyledLink to="#"> Video </StyledLink> </p>
-        <p> <StyledLink to="#"> Projects </StyledLink> </p>
+        {UnderTheButtonLinks.map((link, idx) => {
+          if (idx === 0 ) {
+            return (
+              <p><strong><StyledNonSlug href={link.slug}>{link.section}</StyledNonSlug></strong></p>
+            )
+          } else {
+            return (
+              <p><StyledNonSlug href={link.slug}>{link.section}</StyledNonSlug></p>
+            )
+          }
+        })}
       </Col>
     </Row>
     <p style={{ textAlign: 'center', marginTop: '2em' }}> Copyright &copy; 2020 The Daily Pennsylvanian, Inc. All Rights Reserved.</p>
---
+++ src/constants.ts
@@ -0,0 +1,238 @@
+interface ILink {
+  section: string,
+  slug: string
+}
+
+export const AboutLinks: ILink[] = [
+  {
+    section: 'About',
+    slug: '/page/about',
+  },
+  {
+    section: 'About Us',
+    slug: '/page/about',
+  },
+  {
+    section: 'Archives',
+    slug: '/archives',
+  },
+  {
+    section: 'Staff',
+    slug: '/page/staff_directory',
+  },
+  {
+    section: 'Advertise',
+    slug: '/page/advertising',
+  },
+  {
+    section: 'Donate',
+    slug: '/page/donate',
+  },
+  {
+    section: 'Apparel Store',
+    slug: '/page/apparel-store',
+  },
+  {
+    section: 'Alumni Association',
+    slug: '/page/alumni-association',
+  },
+  {
+    section: 'Privacy Policy',
+    slug: '/page/privacy',
+  },
+  {
+    section: 'Terms of Service',
+    slug: '/page/terms',
+  },
+  {
+    section: 'Contact Us',
+    slug: '/page/contact',
+  },
+]
+
+export const NewsLinks: ILink[] = [
+  {
+    section: 'News',
+    slug: '/section/news',
+  },
+  {
+    section: 'Academics',
+    slug: '/section/academics',
+  },
+  {
+    section: 'Administration',
+    slug: '/section/administration',
+  },
+  {
+    section: 'Admissions',
+    slug: '/section/admissions',
+  },
+  {
+    section: 'Crime',
+    slug: '/section/crime',
+  },
+  {
+    section: 'Politics',
+    slug: '/section/politics',
+  },
+  {
+    section: 'Philadelphia',
+    slug: '/section/philadelphia',
+  },
+  {
+    section: 'Student Life',
+    slug: '/section/student-life',
+  },
+  {
+    section: 'Tech',
+    slug: '/section/tech',
+  },
+]
+
+export const SportsLinks: ILink[] = [
+  {
+    section: 'Sports',
+    slug: '/section/sports',
+  },
+  {
+    section: 'Men\'s Basketball',
+    slug: '/section/mens-hoops',
+  },
+  {
+    section: 'Women\'s Basketball',
+    slug: '/section/womens-hoops',
+  },
+  {
+    section: 'Wrestling',
+    slug: '/section/wrestling',
+  },
+  {
+    section: 'Men\'s Squash',
+    slug: '/section/mens-squash',
+  },
+  {
+    section: 'Women\'s Squash',
+    slug: '/section/womens-squash',
+  },
+  {
+    section: 'Men\'s Swimming',
+    slug: '/section/mens-swimming',
+  },
+  {
+    section: 'Women\'s Swimming',
+    slug: '/section/womens-swimming',
+  },
+  {
+    section: 'Track & Field',
+    slug: '/section/track',
+  },
+  {
+    section: 'Gymnastics',
+    slug: '/section/gymnastics',
+  },
+  {
+    section: 'Fencing',
+    slug: '/section/fencing',
+  },
+]
+
+export const OpinionLinks: ILink[] = [
+  {
+    section: 'Opinion',
+    slug: '/section/opinion',
+  },
+  {
+    section: 'Staff Editorials',
+    slug: '/section/editorials',
+  },
+  {
+    section: 'Opinion Columns',
+    slug: '/section/columns',
+  },
+  {
+    section: 'Letters to the Editor',
+    slug: '/section/letters',
+  },
+  {
+    section: 'Opinion Art',
+    slug: '/section/opart',
+  },
+  {
+    section: 'Guest Columns',
+    slug: '/section/guest-columns',
+  },
+  {
+    section: 'Submit a Column',
+    slug: '/section/opinion-submissions',
+  },
+]
+
+export const StreetLinks: ILink[] = [
+  {
+    section: '34th Street',
+    slug: 'www.34st.com',
+  },
+  {
+    section: 'Features',
+    slug: 'www.34st.com/section/features',
+  },
+  {
+    section: 'Ego',
+    slug: 'www.34st.com/section/ego',
+  },
+  {
+    section: 'Word on the Street',
+    slug: 'www.34st.com/section/word-on-the-street',
+  },
+  {
+    section: 'Film & TV',
+    slug: 'www.34st.com/section/film',
+  },
+  {
+    section: 'Music',
+    slug: 'www.34st.com/section/music',
+  },
+  {
+    section: 'Arts',
+    slug: 'www.34st.com/section/arts',
+  },
+  {
+    section: 'Style',
+    slug: 'www.34st.com/section/style',
+  },
+  {
+    section: 'Lastpage',
+    slug: 'www.34st.com/section/lastpage',
+  },
+  {
+    section: 'Multimedia',
+    slug: 'www.34st.com/multimedia',
+  },
+  {
+    section: 'Projects',
+    slug: 'projects.34st.com',
+  },
+]
+
+export const UnderTheButtonLinks: ILink[] = [
+  {
+    section: 'Under the Button',
+    slug: 'www.underthebutton.com',
+  },
+  {
+    section: 'News',
+    slug: 'www.underthebutton.com/section/news',
+  },
+  {
+    section: 'Opinion',
+    slug: 'www.underthebutton.com/section/opinion',
+  },
+  {
+    section: 'Video',
+    slug: 'www.underthebutton.com/section/video',
+  },
+  {
+    section: 'Projects',
+    slug: 'projects.underthebutton.com',
+  },
+]
