65ff9fa804b384004f52ce4f7d06c6a494e8b0ef
Chirality <danieltaox@gmail.com>
2020-10-26 17:06:28 -0400

    add basic Article graphql schema to dev server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,51 @@
 const express = require('express')
 const axios = require('axios')
-const app = express()
+const { ApolloServer, PubSub, gql } = require('apollo-server-express')
+const { RESTDataSource } = require('apollo-datasource-rest')
+
+const typeDefs = gql`
+    type Subscription {
+        articleEdited: Article
+    }
+
+    type Query {
+        article(slug: String) : Article
+        articles: [Article]
+    }
+
+    type Article {
+        slug: String
+        headline: String
+        abstract: String
+        content: String
+    }
+`
+class ContentAPI extends RESTDataSource {
+    constructor(){
+        super();
+    }
+
+    async getArticle(slug) {
+        const data = await this.get(`https://www.thedp.com/article/${slug}.json`)
+        return data.article
+    }
+
+    async getArticles() {
+        const data = await this.get(`https://www.thedp.com/section/news.json`)
+        return data.articles;
+    }
+}
+
+const resolvers = {
+    Query: {
+        article: async (_, { slug }, { dataSources }) => dataSources.contentAPI.getArticle(slug),
+        articles: async (_, _args, { dataSources }) => dataSources.contentAPI.getArticles()
+    }
+}
+
+// REST routes
+const app = express();
+app.use(express.json());
 
 // Middleware to disable CORS
 app.get('/*', (_, res, next) => {
@@ -20,4 +65,21 @@ app.get('/fetch', async ({ query: { url = '' } }, res) => {
   }
 })
 
-app.listen(5000, () => console.log('Server running on port 5000'))
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            contentAPI: new ContentAPI()
+        }
+    }
+})
+
+server.applyMiddleware({ app })
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(5000, () => {
+    console.log('Server ready at port 5000')
+    console.log('Subscriptions ready at ws://localhost:5000')
+})
